67569231683de758f8d8fe807ac21319456a1abf
belisky <denoblesnobility2@gmail.com>
2022-12-22 16:26:13 +0000

    push2ecr

--- push2ecr.sh
+++ push2ecr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 644435390668.dkr.ecr.us-east-1.amazonaws.
-docker tag ts_ted_search:latest 644435390668.dkr.ecr.us-east-1.amazonaws.com/nobel_tedsearch:latest
-docker tag ts_ted_nginx:latest 644435390668.dkr.ecr.us-east-1.amazonaws.com/nobel_ted_nginx:latest
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 644435390668.dkr.ecr.us-east-1.amazonaws.com
+docker tag ts-ted_search:latest 644435390668.dkr.ecr.us-east-1.amazonaws.com/nobel_tedsearch:latest
+docker tag ts-ted_nginx:latest 644435390668.dkr.ecr.us-east-1.amazonaws.com/nobel_ted_nginx:latest
 docker push 644435390668.dkr.ecr.us-east-1.amazonaws.com/nobel_tedsearch:latest
 docker push 644435390668.dkr.ecr.us-east-1.amazonaws.com/nobel_ted_nginx:latest
