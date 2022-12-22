a7ba4f315b372811bff6aaa141ff2ac994d20506
belisky <denoblesnobility2@gmail.com>
2022-12-22 22:10:38 +0000

    main

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ec2" {
   ami           = "ami-0574da719dca65348" 
 
      user_data       = file("install_docker.sh")
+    security_groups = ["${aws_security_group.sg.id}"] 
 
     connection {
       type        = "ssh"
@@ -13,8 +14,7 @@ resource "aws_instance" "ec2" {
    provisioner "file" {
         source      = "./docker-compose-ec2.yml"
         destination = "/home/ubuntu/docker-compose.yml"
-       
-
+     
    }
     provisioner "remote-exec" {
     inline = [
@@ -22,7 +22,45 @@ resource "aws_instance" "ec2" {
       "sudo docker-compose -p ts up -d --wait",
     ]
 
-    } 
+    }
+}
+resource "aws_security_group" "sg" {
+  name        = "webSG"
+  description = "Allow ssh  inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 8083
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 8084
+    to_port     = 8084
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+
+  }
 }
  
  
