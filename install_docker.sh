8d3d57b529e0844015134b3af24fa8d6951f37ab
belisky <denoblesnobility2@gmail.com>
2022-12-23 14:13:39 +0000

    main

--- install_docker.sh
+++ install_docker.sh
@@ -7,7 +7,7 @@ sudo apt-get install \
     lsb-release -y
 
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
