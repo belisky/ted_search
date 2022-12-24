dd4a81582aad1c61606ce15d4312cc73e1866928
belisky <denoblesnobility2@gmail.com>
2022-12-24 19:52:28 +0000

    fixes

--- install_docker.sh
+++ install_docker.sh
@@ -9,6 +9,7 @@ sudo apt-get install \
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 
+sudo apt-get update
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -16,8 +17,9 @@ echo \
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-get update
 sudo apt install unzip zip -y
-
+sudo apt-get update
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
 sudo ./aws/install 
